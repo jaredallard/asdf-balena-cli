c23b5b1c64c5a6bb69c356033e14bf055423cf6f
Jared Allard <jaredallard@users.noreply.github.com>
2025-06-29 15:30:41 -0700

    debug

--- lib/utils.bash
+++ lib/utils.bash
@@ -86,7 +86,7 @@ download_release() {
 install_version() {
   local install_type="$1"
   local version="$2"
-  local install_path="${3%/bin}/bin"
+  local install_path="${3%/bin}"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-$TOOL_NAME supports release installs only"
@@ -94,10 +94,10 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cp -rv "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
     local tool_cmd="$TOOL_TEST"
-    "$install_path/$tool_cmd" --help &>/dev/null || fail "Expected $install_path/$tool_cmd --help to succeed"
+    "$install_path/bin/$tool_cmd" --help &>/dev/null || fail "Expected $install_path/bin/$tool_cmd --help to succeed"
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
